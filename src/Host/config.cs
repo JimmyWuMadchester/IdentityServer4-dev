1e61f5d9b5607050e64d8dea4697c9cb2f29aa73
Yulin Wu (RFS/Inv/IC Ops <yulin.wu@towerswatson.com>
2016-11-23 12:09:41 +0000

    YW's Configuration

---
+++ src/Host/config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace Host
+{
+    public class config
+    {
+        // Scopes define the resources in your system that you want to protect, e.g. APIs
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API"
+                }
+            };
+        }
+
+        // Define clients that can access the protected resources
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
